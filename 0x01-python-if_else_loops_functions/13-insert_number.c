e7d3c32e4ddb494fad7cd57fc32f1aab60fc94c2
David Peralta <924@holbertonschool.com>
2019-09-10 16:55:59 -0500

    fix 13-insert_number.c edge case negative number

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,5 +1,25 @@
 #include "lists.h"
 #include <stdlib.h>
+
+/**
+ * add_nodeint - adds a new node at the beginning of a listint_t list.
+ * @head: pointer to first node.
+ * @n: given int to be added.
+ * Return: the address of the new element, or NULL if it failed.
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *temp;
+
+	temp = malloc(sizeof(listint_t));
+	if (temp == NULL)
+		return (NULL);
+	temp->n = n;
+	temp->next = *head;
+	*head = temp;
+	return (temp);
+}
+
 /**
  * insert_node - inserts a number into a sorted singly linked list.
  * @head: pointer to head of list.
@@ -19,7 +39,13 @@ listint_t *insert_node(listint_t **head, int number)
 		new = add_nodeint_end(head, number);
 		return (new);
 	}
-	for (current = *head; current; current = current->next)
+	current = *head;
+	if (current->n > number)
+	{
+		new = add_nodeint(head, number);
+		return (new);
+	}
+	for (; current; current = current->next)
 	{
 		if (current->next == NULL)
 		{
