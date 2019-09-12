c64b160ffa07442af10d2f7ecf573d1945b475c1
David Peralta <924@holbertonschool.com>
2019-09-12 13:50:47 -0500

    fix 4 condition not execute

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import hidden_4
-lis = dir(hidden_4)
-for names in lis:
-    if names[0:2] != "__":
-        print(names)
+if __name__ == "__main__":
+    import hidden_4
+    lis = dir(hidden_4)
+    for names in lis:
+        if names[0:2] != "__":
+            print(names)
