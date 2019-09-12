243d54f517b0a771c6fd258cd1830391a0aea5ba
David Peralta <924@holbertonschool.com>
2019-09-12 12:24:41 -0500

    fix 2-args.py condition when 1 argument only

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,9 @@ if __name__ == "__main__":
     ar_num = len(sys.argv) - 1
     if ar_num == 0:
         print("{} arguments.".format(ar_num))
+    elif ar_num == 1:
+        print("{} argument:".format(ar_num))
+        print("{}: {}".format(1, sys.argv[1]))
     else:
         print("{} arguments:".format(ar_num))
         n = 1
