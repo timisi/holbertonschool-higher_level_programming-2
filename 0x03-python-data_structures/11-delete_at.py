9d675d91fc3e842d336ba287f578fcbcbc55a720
David Peralta <924@holbertonschool.com>
2019-09-17 15:09:29 -0500

    add 11-delete_at.py

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if not my_list:
+        return []
+    else:
+        if idx > len(my_list) or idx < 0:
+            return my_list
+        else:
+            del my_list[idx]
+        return my_list
