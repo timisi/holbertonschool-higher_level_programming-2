cfff2ac0984fdaf788dc74e4701345b96ef7cdb7
David Peralta <924@holbertonschool.com>
2019-09-17 12:39:49 -0500

    3 fix condition

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    if my_list == []:
+    if not my_list:
         pass
     for i in reversed(range(len(my_list))):
         print("{:d}".format(my_list[i]))
