fbaf0e187ae1fe6dea40b282ebd073e1bee54418
David Peralta <924@holbertonschool.com>
2019-09-17 15:14:17 -0500

    fix 11-delete edge case

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -3,8 +3,8 @@ def delete_at(my_list=[], idx=0):
     if not my_list:
         return []
     else:
-        if idx > len(my_list) or idx < 0:
+        if idx > len(my_list) - 1 or idx < 0:
             return my_list
         else:
             del my_list[idx]
-        return my_list
+            return my_list
