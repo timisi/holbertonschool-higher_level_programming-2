0eb3db01ea6efaa5286fda2872589001a7caccc4
David Peralta <924@holbertonschool.com>
2019-09-17 12:53:32 -0500

    add 6-print_matrix file

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,5 @@ def print_matrix_integer(matrix=[[]]):
     else:
         for x in matrix:
             for y in x[:-1]:
-                print("{} ".format(y), end="")
-            print("{}".format(x[-1]))
+                print("{:d} ".format(y), end="")
+            print("{:d}".format(x[-1]))
