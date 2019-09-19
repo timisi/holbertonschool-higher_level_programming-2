eab500b77117278233cd82a321e1e466b8da5da8
David Peralta <924@holbertonschool.com>
2019-09-19 16:34:02 -0500

    fix 102-complex pip8

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     cp_dic = a_dictionary.copy()
-    for ke,va in cp_dic.items():
+    for ke, va in cp_dic.items():
         if va == value:
             del a_dictionary[ke]
     return a_dictionary
