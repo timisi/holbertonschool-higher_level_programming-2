c9369042e3cd7641b2a4f2418915bf56b59a1277
David Peralta <924@holbertonschool.com>
2019-09-19 12:33:06 -0500

    fix pep8 style code

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    replace_f = lambda x : replace if x == search else x
-    new_list = list(map(replace_f, my_list))
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
     return new_list
--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     else:
-        key_name = (sorted(a_dictionary.items(), key=lambda x : x[1], reverse = True))[0][0]
-        return key_name
+        ky_n = (sorted(a_dictionary.items(), key=lambda x: x[1], reverse=True))
+        return ky_n[0][0]
