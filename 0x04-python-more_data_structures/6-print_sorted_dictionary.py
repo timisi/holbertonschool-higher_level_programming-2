2c566ee3842dd74ee00427d0692ca4f7b6832d5a
David Peralta <924@holbertonschool.com>
2019-09-19 12:37:20 -0500

    fix 6-print format output

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     names = sorted(a_dictionary)
     for n in names:
-        print("{} : {}".format(n, a_dictionary[n]))
+        print("{}: {}".format(n, a_dictionary[n]))
