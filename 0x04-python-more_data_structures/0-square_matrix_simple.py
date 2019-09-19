c25834b40f9bca4b245f359cc742d8adfa43ff2f
David Peralta <924@holbertonschool.com>
2019-09-19 12:25:35 -0500

    fix 0-square_matrix_simple.py pip8

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    sqr_fun = lambda x : x ** 2
-    new_matrix =[]
+    new_matrix = []
     for i in range(len(matrix)):
-        new_matrix += [list(map(sqr_fun, matrix[i]))]
-
+        new_matrix += [list(map(lambda x: x ** 2, matrix[i]))]
     return new_matrix
