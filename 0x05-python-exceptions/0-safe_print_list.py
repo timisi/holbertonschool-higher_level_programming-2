1e423630d304cc85fcbd9c2357801e3db478542a
David Peralta <924@holbertonschool.com>
2019-09-30 12:27:28 -0500

    fix condition 0-safe function

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
         for i in range(0, x):
             print("{}".format(my_list[i]), end="")
+            count += 1
         print()
-        return i
+        return count
     except IndexError:
         print()
-        return i
+        return count
