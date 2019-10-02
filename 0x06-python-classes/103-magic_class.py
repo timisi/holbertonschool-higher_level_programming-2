f376b09769eb11f61884886c94e4e43830164e9c
David Peralta <924@holbertonschool.com>
2019-10-01 21:35:25 -0500

    Add 103-magic function

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,21 @@
+class MagicClass:
+    """
+    MagicClass function
+    Args:
+        radius (int or float)
+    Attributes:
+        __radius: private
+    """
+    def __init__(self, radius=0):
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """return the area of the object"""
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """return the circunference of the object"""
+        return 2 * math.pi * self.__radius
