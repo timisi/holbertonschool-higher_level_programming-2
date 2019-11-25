c65f00999dab223b64e6e11697386f3b3f77ba70
david-develop <924@holbertonschool.com>
2019-11-25 17:21:52 -0500

    [Update] max temperture query output name and average ouput name

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,3 +1,3 @@
 -- Script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
 -- Script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
-SELECT state, MAX(value) AS avg_temp FROM temperatures GROUP BY state ORDER BY state;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state;
--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- Script computes the average of a given record
 -- Computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT AVG(score) FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
