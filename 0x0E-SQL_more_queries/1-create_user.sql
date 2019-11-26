1e4d45fc88e80a2b875faab689145e881ea8583c
david-develop <924@holbertonschool.com>
2019-11-26 18:57:42 +0000

    [Add] files

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Script that create an user and set privileges
+-- Creates the MySQL server user user_0d_1, with password user_0d_1_pwd and all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
