639165bff61bcaf21a54986d30448624d539e47a
david-develop <924@holbertonschool.com>
2019-12-13 22:23:59 +0000

    [Add] files of the project

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Module that that lists all states from the database"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=MY_USER,
+        passwd=MY_PASS,
+        db=MY_DB
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    table = cur.fetchall()
+
+    for row in table:
+        print(row)
+
+    cur.close()
+    db.close()
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Module that that lists all cities from the database"""
+
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+
+    MY_USER = sys.argv[1]
+    MY_PASS = sys.argv[2]
+    MY_DB = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        user=MY_USER,
+        passwd=MY_PASS,
+        db=MY_DB
+    )
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities\
+                 JOIN states\
+                 ON states.id = cities.state_id\
+                 WHERE states.name = (%s) ORDER BY cities.id ASC",
+                (state_name,))
+    table = cur.fetchall()
+
+    str_cities = ""
+    for row in table[:-1]:
+        str_cities = str_cities + row[0] + ", "
+    str_cities = str_cities + table[-1][0]
+
+    print(str_cities)
+    cur.close()
+    db.close()
