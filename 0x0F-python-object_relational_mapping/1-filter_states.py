62f1de16245b96d41e16b25851fc1057d35b89e3
david-develop <924@holbertonschool.com>
2019-12-14 17:13:33 +0000

    [Fix] 1 script style and arguments verif

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,22 +8,24 @@ import sys
 
 if __name__ == "__main__":
 
-    MY_USER = sys.argv[1]
-    MY_PASS = sys.argv[2]
-    MY_DB = sys.argv[3]
+    if len(sys.argv) == 4:
+        MY_USER = sys.argv[1]
+        MY_PASS = sys.argv[2]
+        MY_DB = sys.argv[3]
 
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=MY_USER,
-        passwd=MY_PASS,
-        db=MY_DB
-    )
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name REGEXP '^N' ORDER BY states.id ASC")
-    table = cur.fetchall()
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=MY_USER,
+            passwd=MY_PASS,
+            db=MY_DB
+        )
+        cur = db.cursor()
+        cur.execute("SELECT * FROM states WHERE name REGEXP '^N'\
+        ORDER BY states.id ASC")
+        table = cur.fetchall()
 
-    for row in table:
-        print(row)
-    cur.close()
-    db.close()
+        for row in table:
+            print(row)
+        cur.close()
+        db.close()
