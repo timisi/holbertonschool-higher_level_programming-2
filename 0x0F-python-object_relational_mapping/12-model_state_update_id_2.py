c3ad2fc0dee801a2b6996feac6752f82840ba1b8
david-develop <924@holbertonschool.com>
2019-12-14 22:12:01 +0000

    [Add] 12 model query to update

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -13,5 +13,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    session.query(State).filter(State.id == "2").update({State.name: "New Mexico"})
+    session.query(State).filter(State.id == "2").\
+        update({State.name: "New Mexico"})
     session.commit()
