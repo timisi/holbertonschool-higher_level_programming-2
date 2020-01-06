a466279e808e7e4faf162d6099294b89be519168
david-develop <924@holbertonschool.com>
2020-01-06 14:29:29 -0500

    Add peak function

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Function find_peak that finds a peak in a list of unsorted integers."""
+
+def find_peak(list_of_integers):
+    """Function that finds a peak in a list of unsorted integers."""
+
+    if not list_of_integers:
+        return None
+
+    peak = list_of_integers[0]
+
+    for n in list_of_integers:
+        if n > peak:
+            peak = n
+
+    return peak
