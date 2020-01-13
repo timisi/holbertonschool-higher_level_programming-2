3942c421b92975774575615ef30017717affbd35
david-develop <924@holbertonschool.com>
2020-01-13 15:14:37 -0500

    [Add] basic authentication script for github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Python script that takes in a string and sends a search request to the Star
+Wars API
+"""
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com/user'
+    r = requests.get(url, auth=HTTPBasicAuth(sys.argv[1], sys.argv[2]))
+    print(r)
+    json_obj = r.json()
+    id_num = json_obj.get('id')
+    print(id_num)
