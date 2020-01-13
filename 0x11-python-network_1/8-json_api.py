128e43e4c3fe86cbc60af92944ac6255dfd08e91
david-develop <924@holbertonschool.com>
2020-01-13 13:47:51 -0500

    [add] 8-json script

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Python script that takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) > 1:
+        value = sys.argv[1]
+    else:
+        value = ""
+    r = requests.post(url, data={'q': value})
+    try:
+        json_obj = r.json()
+        if not json_obj:
+            print("No result")
+	else:
+            print("[{}] {}".format(json_obj.get('id'), json_obj.get('name')))
+    except ValueError:
+        print("Not a valid JSON")
