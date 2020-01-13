e6f84aa2418916b392d248acbbf4428ca2c96b07
david-develop <924@holbertonschool.com>
2020-01-13 13:12:41 -0500

    [Fix] 3-error import method script

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,9 +8,8 @@ import sys
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
     try:
+        url = sys.argv[1]
         with request.urlopen(url) as response:
             page_info = response.read()
             print(page_info.decode('utf-8'))
