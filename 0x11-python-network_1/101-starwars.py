6240f78245c0b5bad133af36fd0c632fe6a695d3
david-develop <924@holbertonschool.com>
2020-01-13 16:32:38 -0500

    [Add] 101-starwars pagination feature script

---
+++ 0x11-python-network_1/101-starwars.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Python script that takes in a string and sends a search request to the Star
+Wars API
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = 'https://swapi.co/api/people/?search=' + sys.argv[1]
+    r = requests.get(url)
+    json_obj = r.json()
+    page_num = 0
+
+    while json_obj['next'] is not None:
+        if page_num == 0:
+                print("Number of results: {}".format(json_obj.get('count')))
+        else:
+            r = requests.get(json_obj['next'])
+            json_obj = r.json()
+        results = json_obj.get('results')
+        for people in results:
+            print(people.get('name'))
+        page_num += 1
