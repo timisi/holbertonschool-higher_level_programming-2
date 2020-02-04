bdd8743f3a1b829e8758b7c6a51d8a9e926de7f7
david-develop <924@holbertonschool.com>
2020-02-04 15:03:50 -0500

    [Fix] 11-second script for negative numbers case

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,5 +4,5 @@ if (process.argv.length < 4) {
   console.log('0');
 } else {
   const arrNum = process.argv.slice(2).map(Number);
-  console.log(arrNum.sort()[arrNum.length - 2]);
+  console.log(arrNum.sort((a, b) => a - b)[arrNum.length - 2]);
 }
