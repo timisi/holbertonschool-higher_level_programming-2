efed6aef6079c2bde31f5a9186ceafdab8a7f552
david-develop <924@holbertonschool.com>
2020-02-10 18:22:17 -0500

    [Fix] import file definition

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
+
 const list = require('./100-data').list;
 
-const map1 = list.map(function (num, index) {
+const newList = list.map(function (num, index) {
   return num * index;
 });
 
 console.log(list);
-console.log(map1);
+console.log(newList);
