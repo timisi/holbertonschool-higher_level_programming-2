af2854237ae76db3630365e17ba75a9c333d1426
david-develop <924@holbertonschool.com>
2020-02-10 16:54:09 -0500

    [Fix] 5-square condition atribute set

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -5,8 +5,5 @@ const Rectangle = require('./4-rectangle');
 module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
-    if (!(size <= 0) && size) {
-      this.size = size;
-    }
   }
 };
