c0df509108c019dcf75d602ac2b17709dcb5e70d
david-develop <924@holbertonschool.com>
2020-02-10 17:14:17 -0500

    [Add] 7-ocurrences script and add executable permissions to file, other scripst empty files

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,4 +6,13 @@ module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
+
+  charPrint (c) {
+    if (!c) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
 };
