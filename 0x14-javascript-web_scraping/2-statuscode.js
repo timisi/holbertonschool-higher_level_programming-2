4d0d17c0a80406840c652d7ce3932ce9ba7495cf
david-develop <924@holbertonschool.com>
2020-02-11 12:44:00 -0500

    [Fix] error condition script 0 and 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,7 @@ const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (error) {
     console.error(error); // Print the error if one occurred
+  } else {
+    console.log('code:', response && response.statusCode); // Print the response status code if a response was received
   }
-  console.log('code:', response && response.statusCode); // Print the response status code if a response was received
 });
