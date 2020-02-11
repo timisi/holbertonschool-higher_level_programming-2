5a15dbb432bcb6ddfb5c864b6b20333441f8c891
david-develop <924@holbertonschool.com>
2020-02-11 12:52:57 -0500

    [Fix] 4 starswars api error condition

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,15 +5,16 @@ const urlApi = process.argv[2];
 
 request(urlApi, function (error, response, body) {
   if (error) {
-    console.error(error); // Print the error if one occurred
-  }
-  const jsonObj = JSON.parse(body).results;
-  const wedgeUrl = 'https://swapi.co/api/people/18/';
-  let count = 0;
-  for (let i = 0; i < jsonObj.length; i++) {
-    if (jsonObj[i].characters.includes(wedgeUrl)) {
-      count += 1;
+    console.log(error); // Print the error if one occurred
+  } else {
+    const jsonObj = JSON.parse(body).results;
+    const wedgeUrl = 'https://swapi.co/api/people/18/';
+    let count = 0;
+    for (let i = 0; i < jsonObj.length; i++) {
+      if (jsonObj[i].characters.includes(wedgeUrl)) {
+        count += 1;
+      }
     }
+    console.log(count);
   }
-  console.log(count);
 });
