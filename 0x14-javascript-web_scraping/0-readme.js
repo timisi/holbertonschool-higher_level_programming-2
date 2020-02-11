eab0400db780c6348e267494d606add4a6353356
david-develop <924@holbertonschool.com>
2020-02-11 12:45:43 -0500

    [Fix] 0-readme.js conditions error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    return console.error(err);
+    console.error(err);
   } else {
     console.log(data);
   }
