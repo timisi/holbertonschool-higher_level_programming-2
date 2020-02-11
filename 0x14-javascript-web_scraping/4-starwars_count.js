33feb2753c7a7c9f4b6b71fdd21278b4b044cb16
david-develop <924@holbertonschool.com>
2020-02-11 13:31:13 -0500

    [Update] condition to check for character

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,12 +8,15 @@ request(urlApi, function (error, response, body) {
     console.log(error); // Print the error if one occurred
   } else {
     const jsonObj = JSON.parse(body).results;
-    const wedgeUrl = 'https://swapi.co/api/people/18/';
     let count = 0;
+    let charList;
     for (let i = 0; i < jsonObj.length; i++) {
-      if (jsonObj[i].characters.includes(wedgeUrl)) {
-        count += 1;
-      }
+      charList = jsonObj[i].characters;
+      charList.forEach(element => {
+        if (element.search('/18/') > 0) {
+          count += 1;
+        }
+      });
     }
     console.log(count);
   }
